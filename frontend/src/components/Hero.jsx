1759b8a4324722f70bd1fef3e64bf74c3f7ee688
jruttan1 <jruttan3@uwo.ca>
2026-03-30 21:37:11 -0400

    feat: add links to hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
                 i wanted to build this as a thesis on intentional design, a learning project, and most importantly a tool i actually needed. as someone with adhd, i noticed everytime i wanted to write something down i’d either lose it in apple notes, or get paralyzed by a notion template. so i built this instead
             </div>
             <div className = 'links'>
-                blog  <span> • </span> github
+                <a href = 'https://medium.com/@jackjr.ruttan' target="_blank" rel="noreferrer">blog</a> • <a href ='https://github.com/jruttan1/eso' target="_blank" rel="noreferrer">github</a>
             </div>
         </div>
     )
